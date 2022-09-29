e5fe8e2524e90bdb129ca9bcb5e70566c5f3d869
dmnitprof <handleSearchQuery>
2022-09-29 20:21:43 +0300

    create app

---
+++ components/Navigation.tsx
@@ -0,0 +1,7 @@
+export default function Navigation () {
+    return (
+        <nav class="flex h-[50px] absolute top-0 left-0 right-0 bg-blue-300 text-white items-center justify-between px-4">
+            <h1 class="text-lg pl-8">Fresh App</h1>
+        </nav>
+    )
+}
